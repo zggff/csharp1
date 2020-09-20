cdae353b254cd8d12bd08f14bf81fda11721459b
zggff <gigamaximwachau@gmail.com>
2020-09-20 16:46:44 +0000

    hrllo:

--- src/main.cs
+++ src/main.cs
@@ -1,12 +1,12 @@
 using System;
- 
-namespace helloapp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+namespace program{
+    class Program{
+        public static void Main(string[] args){
+            //формат массива - [12;342;423;412;12.2]
+            foreach(string arg in args){
+                Console.WriteLine(arg);
+            }
         }
     }
-}
+} 
